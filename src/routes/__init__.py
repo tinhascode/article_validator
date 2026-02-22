9f7409fb11ecde2ef9051d7716b60d3e0656060f
tinhascode <victormartinssantos.work@gmail.com>
2026-02-22 15:02:59 -0300

    feat: add role_router to the routers list in __init__.py

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -4,10 +4,12 @@ from __future__ import annotations
 from fastapi import FastAPI
 
 from .user_routes import user_router
+from .role_routes import role_router
 
 
 routers = [
     user_router,
+    role_router
 ]
 
 
