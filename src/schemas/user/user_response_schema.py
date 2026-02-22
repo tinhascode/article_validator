61ce38357fe93eff8b68694fa807288969940891
tinhascode <victormartinssantos.work@gmail.com>
2026-02-22 14:34:03 -0300

    style: update import statement for UserReadSchema to use absolute path

--- src/schemas/user/user_response_schema.py
+++ src/schemas/user/user_response_schema.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from src.schemas.base import BaseSchema
 
-from .user_read_schema import UserReadSchema
+from src.schemas.user.user_read_schema import UserReadSchema
 
 
 class UserResponseSchema(BaseSchema):
