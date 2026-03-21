a695e359438061fdf0d657e6cb6913dc4fb10d75
tinhascode <victormartinssantos.work@gmail.com>
2026-03-21 12:29:36 -0300

    refactor: remove unused token_service dependency from AuthRoutes methods

--- src/routes/auth_routes.py
+++ src/routes/auth_routes.py
@@ -5,7 +5,6 @@ from fastapi.security import OAuth2PasswordRequestForm
 
 from src.schemas.auth.access_token_response_schema import AccessTokenResponseSchema
 from src.services.auth_service import get_auth_service, AuthService
-from src.services.token_service import get_token_service, TokenService
 
 
 class AuthRoutes:
@@ -21,26 +20,16 @@ class AuthRoutes:
         request: Request = None,
         response: Response = None,
         auth_service: AuthService = Depends(get_auth_service),
-        token_service: TokenService = Depends(get_token_service),
     ):
-        return auth_service.perform_login(
-            form=form,
-            request=request,
-            response=response,
-        )
+        return auth_service.perform_login(form, request=request, response=response)
 
     async def refresh(
         self,
         request: Request,
         response: Response,
         auth_service: AuthService = Depends(get_auth_service),
-        token_service: TokenService = Depends(get_token_service),
     ):
-        return auth_service.perform_refresh(
-            request=request,
-            response=response,
-            token_service=token_service
-        )
+        return auth_service.perform_refresh(request=request, response=response)
 
 
 auth_router = AuthRoutes().router
